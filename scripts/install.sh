c4070d78f2d579d651446c35214f2a1e9ebd4a00
Alan Jackson <alan.jackson@se.com>
2021-10-08 20:51:01 +0100

    add verboses

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,15 +16,27 @@ mkdir -p mnt
 mkdir -p mnt/fat32
 mkdir -p mnt/ext4
 
-sudo mount ${1}1 mnt/fat32
-sudo mount ${1}2 mnt/ext4
+echo "Mounting..."
 
-sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=mnt/ext4 modules_install
+sudo mount -v ${1}1 mnt/fat32
+sudo mount -v ${1}2 mnt/ext4
 
-sudo cp mnt/fat32/$KERNEL.img mnt/fat32/$KERNEL-backup.img
-sudo cp arch/arm64/boot/Image mnt/fat32/$KERNEL.img
-sudo cp arch/arm64/boot/dts/broadcom/*.dtb mnt/fat32/
-sudo cp arch/arm64/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
-sudo cp arch/arm64/boot/dts/overlays/README mnt/fat32/overlays/
-sudo umount mnt/fat32
-sudo umount mnt/ext4
+echo "Installing modules..."
+#sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=mnt/ext4 modules_install
+
+echo "Copying images..."
+#sudo cp -v mnt/fat32/$KERNEL.img mnt/fat32/$KERNEL-backup.img
+#sudo cp -v arch/arm64/boot/Image mnt/fat32/$KERNEL.img
+
+echo "Copying DTBs..."
+sudo cp -v arch/arm64/boot/dts/broadcom/*.dtb mnt/fat32/
+sudo cp -v arch/arm64/boot/dts/overlays/*.dtb* mnt/fat32/overlays/
+
+echo "Copying README..."
+sudo cp -v arch/arm64/boot/dts/overlays/README mnt/fat32/overlays/
+
+echo "Unmounting..."
+sudo umount -v mnt/fat32
+sudo umount -v mnt/ext4
+
+echo "DONE."
