95b4f4545d591bdd500a9b2faf327605fbe938b1
Alan Jackson <alan.jackson@se.com>
2021-10-08 21:43:59 +0100

    Add code for LED switch on and off with push-button

--- drivers/hello/hello.c
+++ drivers/hello/hello.c
@@ -1,49 +1,100 @@
 /**
- * @file    hello.c
- * @author  Derek Molloy
- * @date    6 November 2015
- * @version 0.1
- * @brief  An introductory "Hello World!" loadable kernel module (LKM)
- * that can display a message in the /var/log/kern.log file when the
- * module is loaded and removed. The module can accept an argument when
- * it is loaded -- the name, which appears in the kernel log files.
-*/
+ * @file   gpio_test.c
+ * @author Derek Molloy
+ * @date   8 November 2015
+ * @brief  A kernel module for controlling a GPIO LED/button pair. The
+ * device mounts an LED and pushbutton via sysfs /sys/class/gpio/gpio17
+ * and gpio27 respectively. */
 
-#include <linux/init.h>      // macros to mark up functions e.g. __init
-#include <linux/module.h>    // core header for loading LKMs
-#include <linux/kernel.h>    // contains kernel types, macros, functions
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>                 // for the GPIO functions
+#include <linux/interrupt.h>            // for the IRQ code
 
-MODULE_LICENSE("GPL");       // the license type (affects behavior)
-MODULE_AUTHOR("Derek Molloy");  // The author visible with modinfo
-MODULE_DESCRIPTION("A simple Linux LKM for the RPi."); // desc.
-MODULE_VERSION("0.1");       // the version of the module
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Derek Molloy");
+MODULE_DESCRIPTION("A Button/LED test driver for the RPi");
+MODULE_VERSION("0.1");
 
-static char *name = "world"; // example LKM argument default is "world"
-// param description charp = char pointer, defaults to "world"
-module_param(name, charp, S_IRUGO); // S_IRUGO can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");
+static unsigned int gpioLED = 17;       // pin 11 (GPIO17)
+static unsigned int gpioButton = 27;    // pin 13 (GPIO27)
+static unsigned int irqNumber;          // share IRQ num within file
+static unsigned int numberPresses = 0;  // store number of presses
+static bool	    ledOn = 0;          // used to invert state of LED
 
-/** @brief The LKM initialization function
- * The static keyword restricts the visibility of the function to within
- * this C file. The __init macro means that for a built-in driver (not
- * a LKM) the function is only used at initialization time and that it
- * can be discarded and its memory freed up after that point.
- * @return returns 0 if successful  */
-static int __init helloERPi_init(void) {
-   printk(KERN_INFO "ERPi: Hello %s from the RPi LKM!\n", name);
-   return 0;
+// prototype for the custom IRQ handler function, function below
+static irq_handler_t  erpi_gpio_irq_handler(unsigned int irq, void
+                                    *dev_id, struct pt_regs *regs);
+
+/** @brief The LKM initialization function */
+static int __init erpi_gpio_init(void){
+   int result = 0;
+   printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
+   if (!gpio_is_valid(gpioLED)){
+      printk(KERN_INFO "GPIO_TEST: invalid LED GPIO\n");
+      return -ENODEV;
+   }
+   ledOn = true;
+   gpio_request(gpioLED, "sysfs");          // request LED GPIO
+   gpio_direction_output(gpioLED, ledOn);   // set in output mode and on
+// gpio_set_value(gpioLED, ledOn);          // not required
+   gpio_export(gpioLED, false);             // appears in /sys/class/gpio
+			               // false prevents direction change
+   gpio_request(gpioButton, "sysfs");       // set up gpioButton
+   gpio_direction_input(gpioButton);        // set up as input
+//   gpio_set_debounce(gpioButton, 200);      // debounce delay of 200ms
+   gpio_export(gpioButton, false);          // appears in /sys/class/gpio
+
+   printk(KERN_INFO "GPIO_TEST: button value is currently: %d\n",
+          gpio_get_value(gpioButton));
+   irqNumber = gpio_to_irq(gpioButton);     // map GPIO to IRQ number
+   printk(KERN_INFO "GPIO_TEST: button mapped to IRQ: %d\n", irqNumber);
+
+   // This next call requests an interrupt line
+   result = request_irq(irqNumber,         // interrupt number requested
+            (irq_handler_t) erpi_gpio_irq_handler, // handler function
+            IRQF_TRIGGER_RISING,  // on rising edge (press, not release)
+            "erpi_gpio_handler",  // used in /proc/interrupts
+            NULL);                // *dev_id for shared interrupt lines
+   printk(KERN_INFO "GPIO_TEST: IRQ request result is: %d\n", result);
+   return result;
 }
 
-/** @brief The LKM cleanup function
- * Similar to the initialization function, it is static. The __exit
- * macro notifies that if this code is used for a built-in driver (not
- * a LKM) that this function is not required.    */
-static void __exit helloERPi_exit(void) {
-   printk(KERN_INFO "ERPi: Goodbye %s from the RPi LKM!\n", name);
+/** @brief The LKM cleanup function  */
+static void __exit erpi_gpio_exit(void){
+   printk(KERN_INFO "GPIO_TEST: button value is currently: %d\n",
+          gpio_get_value(gpioButton));
+   printk(KERN_INFO "GPIO_TEST: pressed %d times\n", numberPresses);
+   gpio_set_value(gpioLED, 0);    // turn the LED off
+   gpio_unexport(gpioLED);        // unexport the LED GPIO
+   free_irq(irqNumber, NULL);     // free the IRQ number, no *dev_id
+   gpio_unexport(gpioButton);     // unexport the Button GPIO
+   gpio_free(gpioLED);            // free the LED GPIO
+   gpio_free(gpioButton);         // free the Button GPIO
+   printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
 }
 
-/** @brief A module must use the module_init() module_exit() macros from
- * linux/init.h, which identify the initialization function at insertion
- * time and the cleanup function (as listed above).    */
-module_init(helloERPi_init);
-module_exit(helloERPi_exit);
+/** @brief The GPIO IRQ Handler function
+ * A custom interrupt handler that is attached to the GPIO. The same
+ * interrupt handler cannot be invoked concurrently as the line is
+ * masked out until the function is complete. This function is static
+ * as it should not be invoked directly from outside of this file.
+ * @param irq    the IRQ number associated with the GPIO
+ * @param dev_id the *dev_id that is provided - used to identify
+ * which device caused the interrupt. Not used here.
+ * @param regs   h/w specific register values -used for debugging.
+ * return returns IRQ_HANDLED if successful - return IRQ_NONE otherwise.
+ */
+static irq_handler_t erpi_gpio_irq_handler(unsigned int irq,
+                        void *dev_id, struct pt_regs *regs){
+   ledOn = !ledOn;                     // invert the LED state
+   gpio_set_value(gpioLED, ledOn);     // set LED accordingly
+   printk(KERN_INFO "GPIO_TEST: Interrupt! (button is %d)\n",
+          gpio_get_value(gpioButton));
+   numberPresses++;                    // global counter
+   return (irq_handler_t) IRQ_HANDLED; // announce IRQ handled
+}
+
+module_init(erpi_gpio_init);
+module_exit(erpi_gpio_exit);
